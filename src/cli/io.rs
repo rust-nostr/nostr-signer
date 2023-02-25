caebb14f17297a6108f73d67a68618f1061df6fc
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-02-25 17:58:08 +0100

    Ask for secret key and use `EventBuilder::nostr_connect`

--- src/cli/io.rs
+++ src/cli/io.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2023 Yuki Kishimoto
 // Distributed under the MIT software license
 
-use dialoguer::{Confirm, Input};
+use dialoguer::{Confirm, Input, Password};
 use nostr_sdk::Result;
 
 pub fn get_input<S>(prompt: S) -> Result<String>
@@ -11,11 +11,11 @@ where
     Ok(Input::new().with_prompt(prompt).interact_text()?)
 }
 
-/* pub fn get_password() -> Result<String> {
-    Ok(Password::new().with_prompt("Password").interact()?)
+pub fn get_secret_key() -> Result<String> {
+    Ok(Password::new().with_prompt("Secret key").interact()?)
 }
 
-pub fn get_password_with_confirmation() -> Result<String> {
+/* pub fn get_password_with_confirmation() -> Result<String> {
     Ok(Password::new()
         .with_prompt("New password")
         .with_confirmation("Confirm password", "Passwords mismatching")
--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ use serde_json::json;
 
 mod cli;
 
-const BECH32_SK: &str = "nsec1dv3n8qff4eujtfy37hu63duujfqv4me5u54a5ucea6jaeqge5jusljqrrk";
-
 #[tokio::main]
 async fn main() -> Result<()> {
+    // Get secret key
+    let secret_key: String = cli::io::get_secret_key()?;
+    let my_keys = Keys::from_sk_str(&secret_key)?;
+
+    // Get Nostr Connect URI
     let uri = cli::io::get_input("Nostr Connect URI")?;
     let uri: NostrConnectURI = NostrConnectURI::from_str(&uri)?;
-
-    let secret_key = SecretKey::from_bech32(BECH32_SK)?;
-    let my_keys = Keys::new(secret_key);
 
     let client = Client::new(&my_keys);
     client.add_relay(uri.relay_url, None).await?;
@@ -26,13 +26,8 @@ async fn main() -> Result<()> {
 
     // Send connect ACK
     let msg = Message::request(Request::Connect(my_keys.public_key()));
-    let content = encrypt(&my_keys.secret_key()?, &uri.public_key, msg.as_json())?;
-    let nip46_event = EventBuilder::new(
-        Kind::NostrConnect,
-        content,
-        &[Tag::PubKey(uri.public_key, None)],
-    )
-    .to_event(&my_keys)?;
+    let nip46_event =
+        EventBuilder::nostr_connect(&my_keys, uri.public_key, msg)?.to_event(&my_keys)?;
     client.send_event(nip46_event).await?;
 
     // Subscribe to `App` events
@@ -86,14 +81,12 @@ async fn main() -> Result<()> {
                             };
 
                             if let Some(res) = res {
-                                let event = EventBuilder::new(
-                                    Kind::NostrConnect,
-                                    encrypt(&my_keys.secret_key()?, &event.pubkey, res.as_json())?,
-                                    &[Tag::PubKey(uri.public_key, None)],
-                                )
-                                .to_event(&my_keys)?;
+                                let event =
+                                    EventBuilder::nostr_connect(&my_keys, event.pubkey, res)?
+                                        .to_event(&my_keys)?;
                                 let id = client.send_event(event).await?;
-                                println!("\nEvent sent: {id}")
+                                println!("\nEvent sent: {id}");
+                                println!("\n###############################################\n");
                             }
                         }
                     }
