9eff5929150c2933cb7c996747511e987d326aae
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-02-25 11:14:08 +0100

    Init signer

---
+++ src/cli/io.rs
@@ -0,0 +1,38 @@
+// Copyright (c) 2023 Yuki Kishimoto
+// Distributed under the MIT software license
+
+use dialoguer::{Confirm, Input};
+use nostr_sdk::Result;
+
+pub fn get_input<S>(prompt: S) -> Result<String>
+where
+    S: Into<String>,
+{
+    Ok(Input::new().with_prompt(prompt).interact_text()?)
+}
+
+/* pub fn get_password() -> Result<String> {
+    Ok(Password::new().with_prompt("Password").interact()?)
+}
+
+pub fn get_password_with_confirmation() -> Result<String> {
+    Ok(Password::new()
+        .with_prompt("New password")
+        .with_confirmation("Confirm password", "Passwords mismatching")
+        .interact()?)
+} */
+
+pub fn ask<S>(prompt: S) -> Result<bool>
+where
+    S: Into<String> + std::marker::Copy,
+{
+    if Confirm::new()
+        .with_prompt(prompt)
+        .default(false)
+        .interact()?
+    {
+        Ok(true)
+    } else {
+        Ok(false)
+    }
+}
---
+++ src/cli/mod.rs
@@ -0,0 +1,4 @@
+// Copyright (c) 2023 Yuki Kishimoto
+// Distributed under the MIT software license
+
+pub mod io;
